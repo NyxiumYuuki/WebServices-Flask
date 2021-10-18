27bee6dbbb896fd625a43db42b1ccef6277914ab
Yûki Vachot <yuki@vachot.fr>
2021-10-18 07:50:53 +0200

    Update

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 33507))
