591a4fd6c5697f7a77c835cffe2874b28cf78687
Yûki Vachot <yuki@vachot.fr>
2021-11-28 21:34:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_route import *
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 33507))
 
-    METEOCONCEPT_TOKEN = None if app.config['ENV'] == 'development' else "e93ee091ba7cd6ce1882fb55f9c030bbbafa0defaa0d8dd4f780b00897b989e1"
-    WEATHERSTACK_TOKEN = None if app.config['ENV'] == 'development' else "e43f819bbbf50da109d7076840cd3f11"
+    METEOCONCEPT_TOKEN = None if app.config['ENV'] == 'development' else None
+    WEATHERSTACK_TOKEN = None if app.config['ENV'] == 'development' else None
 
     # On Linux or MAC 'export METEOCONCEPT_TOKEN=...' (check shell echo $METEOCONCEPT_TOKEN)
     # On Windows 'set METEOCONCEPT_TOKEN=...' (check on Powershell echo $Env:METEOCONCEPT_TOKEN)
